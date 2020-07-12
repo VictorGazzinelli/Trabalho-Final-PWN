2d6b6c5a3b3727c25d36115debda3ba32c8a7bdd
Lucas Viana <lcasviana@gmail.com>
2020-07-12 17:34:30 -0300

    Redirecionar para autenticação

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const UserRepository = require('../respositories/UserRepository');
 
-const router  = express.Router();
+const router = express.Router();
 
-router.get('/', (req , res ) => {
-    res.send("Bem vindo a list 3000");
+router.get('/', (req, res) => {
+  res.redirect('/auth/github');
 });
 
 //router.get('/login', function(req, res) {
 //    res.render('index', { title: 'Meu APP Final Github' });
 //  });
-  
+
 
 module.exports = router;
 
--- ui/src/app/shared/Login.js
+++ ui/src/app/shared/Login.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+
+import GitHub from '../../assets/github.svg';
 
 export default () => (
-  <>
-    Login
-  </>
+  <div className="w-100 h-100 flex justify-center items-center">
+    <a href={`http://localhost:3000/auth/github`} className='link'>
+      <Button variant='contained' size='large'>
+        <img src={GitHub} alt='GitHub' className='h2 w2' />
+        Login
+      </Button>
+    </a>
+  </div>
 );
