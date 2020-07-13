6a6145b817661d61d54cf32780aaeb3a7403d524
VictorGazzinelli <vpezzigc@gmail.com>
2020-07-13 20:37:40 -0300

    Removed ensureAuthentication

--- api/src/routes/list.js
+++ api/src/routes/list.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const ListRepository = require('../respositories/ListRepository');
-const { ensureAuthentication } = require('../config/ensureAuthentication');
+// const { ensureAuthentication } = require('../config/ensureAuthentication');
 
 const router = express.Router();
 
-router.post('/', ensureAuthentication, async (req , res) => {
-    console.log(req.body);
+router.post('/', /*ensureAuthentication,*/ async (req , res) => {
     const List = await ListRepository.insert(req.body.name,req.body.userId)
     res.status(200).json(List);
 });
 
-router.get('/:id', ensureAuthentication, async (req , res) => {
+router.get('/:id', /*ensureAuthentication,*/ async (req , res) => {
     const List = await ListRepository.getAllByUserId(req.params.id)
     res.status(200).json(List);
 });
 
-router.put('/', ensureAuthentication, async (req , res) => {
+router.put('/', /*ensureAuthentication,*/ async (req , res) => {
     const List = await ListRepository.update(req.body.id, req.body.name)
     res.status(200).json(List);
 });
 
-router.delete('/:id', ensureAuthentication, async (req , res) => {
+router.delete('/:id', /*ensureAuthentication,*/ async (req , res) => {
     const ListId = await ListRepository.delete(req.params.id)
     res.status(200).json({id: ListId});
 });
