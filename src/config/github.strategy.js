9192ed1e70d63fd7a0994224bef69cd09fbc81b1
VictorGazzinelli <vpezzigc@gmail.com>
2020-07-20 19:46:43 -0300

    github credentials change

--- src/config/github.strategy.js
+++ src/config/github.strategy.js
@@ -4,9 +4,9 @@ const GitHubStrategy = require("passport-github").Strategy;
 passport.use(
   new GitHubStrategy(
     {
-      clientID: "Iv1.44cc7e7ec3ceaff5",
-      clientSecret: "333974c5a73b41f35eca75f52cc34484c519ae51",
-      callbackURL: "http://localhost:3000/auth/github/callback",
+      clientID: "Iv1.61fd38cbcb2d64d5",
+      clientSecret: "59d430be8932bea3ed33220f0a5fe02f509133f4",
+      callbackURL: "https://pwn-trabalho-final.herokuapp.com/auth/github/callback",
       scope: 'user:email',
     },
     function (accessToken, refreshToken, profile, done) {
