5fc8d28c0e34b2ec875e5068734030a2579a4780
Lucas Viana <lcasviana@gmail.com>
2020-07-21 23:58:51 -0300

    bootstrap

--- src/views/_document.tsx
+++ src/views/_document.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Document, Head, Main } from '@react-ssr/express';
 
+interface Props {
+  crossorigin?: string;
+}
+
+type Link = Props & React.HTMLProps<HTMLButtonElement> & React.HTMLAttributes<HTMLButtonElement>;
+
 export default class extends Document {
   render() {
     return (
@@ -11,14 +17,18 @@ export default class extends Document {
           <link
             rel="stylesheet"
             href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css" />
-          <link
-            rel="stylesheet"
-            href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" />
         </Head>
         <body className="h-100 w-100 pa0 ma0" style={{ background: "#212121" }}>
           <Main />
         </body>
+        <script>
+          const link = document.createElement('link');
+          link.rel = 'stylesheet';
+          link.href = 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css';
+          link.integrity = 'sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk';
+          link.crossOrigin = 'anonymous';
+          document.head.appendChild(link);
+        </script>
       </html>
     );
   }
