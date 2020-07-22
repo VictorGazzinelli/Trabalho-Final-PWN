1274b0c87d33b3e080771092cec64b5686228cad
VictorGazzinelli <vpezzigc@gmail.com>
2020-07-22 20:31:40 -0300

    Test No Server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
-import http from 'http';
 import register from '@react-ssr/express/register';
 dotenv.config();
 
@@ -10,13 +9,8 @@ import { normalizePort, onError} from './utils/index';
   
   const port = normalizePort(process.env.PORT || '3000');
   console.log('Port assigned ->', port);
-  app.set('port', port);
 
   await register(app);
 
-  const server = http.createServer(app);
-
-  server.listen(port);
-  server.on('error', onError);
-  server.on('listening', () => console.log('Application started!'));
+  app.listen(port,() => console.log('Application started!'))
 })();
